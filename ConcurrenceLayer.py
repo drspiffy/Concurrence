5be8c867c975fb7f92500281bdcd04bfe68638d8
Mike Stark <mike.stark@concur.com>
2017-03-06 13:40:40 -0800

    Added activity regularization and constraints

--- ConcurrenceLayer.py
+++ ConcurrenceLayer.py
@@ -1,17 +1,18 @@
 from keras import backend as K
 from keras.engine import InputSpec, Layer
 from keras import initializations
-from keras import regularizers
+from keras import regularizers, constraints
 
 
 class Concurrence(Layer):
-    def __init__(self, weights=None,
-                 init='glorot_uniform',
-                 W_regularizer=None, **kwargs):
+    def __init__(self, weights=None, init='glorot_uniform',
+                 W_regularizer=None, activity_regularizer=None, W_constraint=None, **kwargs):
         self.init = initializations.get(init)
         self.W_regularizer = regularizers.get(W_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.W_constraint = constraints.get(W_constraint)
         self.initial_weights = weights
-        super(Focus, self).__init__(**kwargs)
+        super(Concurrence, self).__init__(**kwargs)
 
     def build(self, input_shape):
         self.input_spec = [InputSpec(shape=input_shape)]
@@ -20,7 +21,8 @@ class Concurrence(Layer):
         self.W = self.add_weight((self.input_dim, 1),
                                  initializer=self.init,
                                  name='{}_W'.format(self.name),
-                                 regularizer=self.W_regularizer)
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
 
         if self.initial_weights is not None:
             self.set_weights(self.initial_weights)
@@ -34,8 +36,10 @@ class Concurrence(Layer):
     def get_config(self):
         config = {'init': self.init.__name__,
                   'W_regularizer': self.W_regularizer.get_config() if self.W_regularizer else None,
-                  'input_dim': self.input_dim}
-        base_config = super(Focus, self).get_config()
+                  'input_dim': self.input_dim,
+                  'activity_regularizer': self.activity_regularizer.get_config() if self.activity_regularizer else None,
+                  'W_constraint': self.W_constraint.get_config() if self.W_constraint else None}
+        base_config = super(Concurrence, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
     def get_output_shape_for(self, input_shape):
